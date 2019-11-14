27db527b6a1a470cd22f531e6a8a0305b446ac27
angsty-duck <solsticedante@gmail.com>
2019-11-14 21:45:33 +0800

    Add /backend/devSetupMysql/sampleData/user.csv, /backend/devSetupMysql/sampleData/grp.csv, /backend/devSetupMysql/sampleData/tool.csv, /backend/devSetupMysql/sampleData/event.csv, /backend/devSetupMysql/sampleData/comm.csv, /backend/devSetupMysql/sampleData/event-comm.csv as sample data for dev; edit /backend/devSetupMysql/setup.sql to include ignore first line for all load data commands

--- backend/devSetupMysql/setup.sql
+++ backend/devSetupMysql/setup.sql
@@ -2,25 +2,25 @@ create database wthack_automation;
 use wthack_automation;
 
 create table `user` (`uid` varchar(36), `name` TEXT, `contact_number` TEXT, `email` TEXT, `gid` varchar(36), `participating` tinyint);
-load data infile './userSampleData.csv' into table `user`;
+load data infile './user.csv' into table `user` ignore 1 lines;
 create trigger user_trigger before insert on `user` for each row set NEW.uid=uuid();
 
 create table `grp` (`gid` varchar(36), `gname` text, `space` text, `categories` text);
-load data infile './grpSampleData.csv' into table `grp`;
+load data infile './grp.csv' into table `grp` ignore 1 lines;
 create trigger grp_trigger before insert on `grp` for each row set NEW.gid=uuid();
 
 create table `tool` (`tid` varchar(36), `on_loan` tinyint, `on_loan_to` varchar(36), `due_date` datetime, `tool_name` text, `description` text);
-load data infile './toolSampleData.csv' into table `tool`;
+load data infile './tool.csv' into table `tool` ignore 1 lines;
 create trigger tool_trigger before insert on `tool` for each row set NEW.tid=uuid();
 
 create table `event` (`eid` varchar(36), `name` text, `start` datetime, `end` datetime, `place` text, `description` text);
-load data infile './eventSampleData.csv' into table `event`;
+load data infile './event.csv' into table `event` ignore 1 lines;
 create trigger event_trigger before insert on `event` for each row set NEW.eid=uuid();
 
 create table `comm` (`cid` varchar(36), `name` text, `contact` text);
-load data infile './commSampleData.csv' into table `comm`;
+load data infile './comm.csv' into table `comm` ignore 1 lines;
 create trigger comm_trigger before insert on `comm` for each row set NEW.cid=uuid();
 
 create table `event-comm` (`ecid` varchar(36), `eid` varchar(36), `cid` varchar(36));
-load data infile './event-commSampleData.csv' into table `event-comm`;
+load data infile './event-comm.csv' into table `event-comm` ignore 1 lines;
 create trigger `event-comm_trigger` before insert on `event-comm` for each row set NEW.ecid=uuid();
