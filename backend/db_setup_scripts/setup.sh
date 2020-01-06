513981b0199fb414da5cbaf0468f5e502b2c6f18
angsty-duck <solsticedante@gmail.com>
2020-01-06 21:46:06 +0800

    no longer have sampledata for the setting up of databses; developers should write custom INSERT statements to just insert rows required for the test they want to run for their endpoints

--- backend/db_setup_scripts/setup.sh
+++ backend/db_setup_scripts/setup.sh
@@ -4,14 +4,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 echo $SCRIPTPATH
 
 
-# copy over the files that will be inserted into mysql server
-cp "${SCRIPTPATH}/sampleData/comm.csv" /var/lib/mysql-files
-cp "${SCRIPTPATH}/sampleData/event_comm.csv" /var/lib/mysql-files
-cp "${SCRIPTPATH}/sampleData/event.csv" /var/lib/mysql-files
-cp "${SCRIPTPATH}/sampleData/grp.csv" /var/lib/mysql-files
-cp "${SCRIPTPATH}/sampleData/tool.csv" /var/lib/mysql-files
-cp "${SCRIPTPATH}/sampleData/user.csv" /var/lib/mysql-files
-
 # expose mysql-server to open internet
 sed "s/bind-address		= 127.0.0.1/bind-address		= 0.0.0.0/g" "/etc/mysql/mysql.conf.d/mysqld.cnf" > "/etc/mysql/mysql.conf.d/mysqld.cnf"
 
--- backend/db_setup_scripts/setup.sql
+++ backend/db_setup_scripts/setup.sql
@@ -19,89 +19,143 @@ FLUSH PRIVILEGES;
 
 -- create and fill up tables for wthack_automation
 use wthack_automation;
-create table `user` (
-	`uid` INT NOT NULL AUTO_INCREMENT,
-	`name` TEXT, 
-	`contact_number` TEXT, 
-	`email` TEXT, 
-	`gid` INT NOT NULL, 
-	`participating` tinyint NOT NULL,
-	PRIMARY KEY (uid)
+create table `user` ( \
+        `user_id` varchar(36) NOT NULL, \
+      	`name` TEXT NOT NULL, \
+      	`contact_number` TEXT NOT NULL, \
+      	`email` TEXT NOT NULL, \
+      	`group_id` varchar(36), \
+      	`participating` tinyint NOT NULL, \
+      	`DoB` datetime NOT NULL, \
+      	`gender` TEXT NOT NULL, \
+      	`nationality` TEXT NOT NULL, \
+      	`category_of_interest` TEXT NOT NULL, \
+      	`technology_of_interest` TEXT NOT NULL, \
+      	`skills` TEXT NOT NULL, \
+      	`organisation` TEXT NOT NULL, \
+      	`designation` TEXT NOT NULL, \
+      	`dietary_pref` TEXT NOT NULL, \
+      	`NoK_name` TEXT NOT NULL, \
+      	`NoK_relationship` TEXT NOT NULL, \
+      	`NoK_contact_number` TEXT NOT NULL, \
+        PRIMARY KEY(user_id) \
 );
+create trigger user_trigger before insert on `user` for each row set @last_uuid=uuid(), NEW.user_id=@last_uuid;
 
-LOAD DATA INFILE '/var/lib/mysql-files/user.csv' INTO TABLE `user`
-FIELDS TERMINATED BY ',' ENCLOSED BY '"' 
-LINES TERMINATED BY '\n' IGNORE 1 LINES;
-
-
-create table `grp` (
-	`gid` INT NOT NULL AUTO_INCREMENT, 
-	`gname` TEXT, 
-	`space` TEXT, 
-	`categories` TEXT,
-	PRIMARY KEY (gid)
+create table `duty_roster` ( \
+          `roster_id` varchar(36) NOT NULL, \
+          `activity_name` text NOT NULL, \
+          `start_datetime` datetime NOT NULL, \
+          `end_datetime` datetime NOT NULL, \
+          `place` text NOT NULL, \
+          `description` text, \
+          PRIMARY KEY(roster_id) \
 );
-load data infile '/var/lib/mysql-files/grp.csv' into table `grp` 
-fields terminated by ',' enclosed by '"'
-lines terminated by '\n' ignore 1 lines;
-
-
-create table `tool` (
-	`tid` INT NOT NULL AUTO_INCREMENT, 
-	`on_loan` tinyint, 
-	`on_loan_to` INT, 
-	`due_date` datetime, 
-	`tool_name` TEXT, 
-	`description` TEXT,
-	PRIMARY KEY (tid)
-);
-load data infile '/var/lib/mysql-files/tool.csv' into table `tool` 
-fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
-ignore 1 lines;
-
-
-create table `event` (
-	`eid` INT NOT NULL AUTO_INCREMENT, 
-	`name` TEXT, 
-	`start` datetime, 
-	`end` datetime, 
-	`place` TEXT, 
-	`description` TEXT,
-	PRIMARY KEY (eid)
-);
-load data infile '/var/lib/mysql-files/event.csv' into table `event` 
-fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
-ignore 1 lines;
-
+create trigger duty_roster_trigger before insert on `duty_roster` for each row set @last_uuid=uuid(), NEW.roster_id=@last_uuid;
 
 create table `comm` (
-	`cid` INT NOT NULL AUTO_INCREMENT, 
-	`name` TEXT, 
-	`contact` TEXT,
-	PRIMARY KEY (cid)
+        `comm_id` varchar(36) NOT NULL, \
+        `name` TEXT NOT NULL, \
+        `contact` TEXT NOT NULL, \
+        PRIMARY KEY (comm_id) \
 );
-load data infile '/var/lib/mysql-files/comm.csv' into table `comm` 
-fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
-ignore 1 lines;
+create trigger comm_trigger before insert on `comm` for each row set @last_uuid=uuid(), NEW.comm_id=@last_uuid;
 
-
-create table `event_comm` (
-	`ecid` INT NOT NULL AUTO_INCREMENT, 
-	`eid` INT, 
-	`cid` INT,
-	PRIMARY KEY (ecid)
+create table `duty_roster_comm` (
+	`duty_roster_comm_id` varchar(36) NOT NULL, \
+        `comm_id` varchar(36) NOT NULL, \
+	`roster_id` varchar(36) NOT NULL, \
+        PRIMARY KEY (comm_id) \
 );
-load data infile '/var/lib/mysql-files/event_comm.csv' into table `event_comm` 
-fields terminated by ','
-enclosed by '"'
-lines terminated by '\n'
-ignore 1 lines;
+create trigger duty_roster_comm_trigger before insert on `duty_roster_comm` for each row set @last_uuid=uuid(), NEW.duty_roster_comm_id=@last_uuid;
+
+create table `event` (
+          `event_id` varchar(36) NOT NULL, \
+          `name` TEXT NOT NULL, \
+          `place` TEXT NOT NULL, \
+          `start_datetime` datetime NOT NULL, \
+          `end_datetime` datetime NOT NULL, \
+          `description` text, \
+          PRIMARY KEY(event_id) \
+);
+create trigger event_trigger before insert on `event` for each row set @last_uuid=uuid(), NEW.event_id=@last_uuid;
+
+create table `consumable` ( \
+          `consumable_id` varchar(36) NOT NULL, \
+          `name` TEXT NOT NULL, \
+          `description` TEXT, \
+          `stock_qty` INT NOT NULL, \
+          `total_qty` INT NOT NULL, \
+          `quota_per_group` INT NOT NULL, \
+          PRIMARY KEY(consumable_id) \
+);
+create trigger consumable_trigger before insert on `consumable` for each row set @last_uuid=uuid(), NEW.consumable_id=@last_uuid;
+
+create table `consumable_group` ( \
+          `consumable_group_id` varchar(36) NOT NULL, \
+          `group_id` varchar(36) NOT NULL, \
+          `consumable_id` varchar(36) NOT NULL, \
+          `qty` INT NOT NULL, \
+          PRIMARY KEY(consumable_group_id) \
+);
+create trigger consumable_group_trigger before insert on `consumable_group` for each row set @last_uuid=uuid(), NEW.consumable_group_id=@last_uuid;
+
+create table `group` ( \
+          `group_id` varchar(36) NOT NULL, \
+          `name` text NOT NULL, \
+          `space` text NOT NULL, \
+          `categories` text NOT NULL, \
+          PRIMARY KEY(group_id) \
+);
+create trigger group_trigger before insert on `group` for each row set @last_uuid=uuid(), NEW.group_id=@last_uuid;
+
+create table `tool` ( \
+          `tool_id` varchar(36) NOT NULL, \
+          `status` TEXT NOT NULL, \
+          `name` text NOT NULL, \
+          `description` text, \
+	  `latest_loan` varchar(36), \
+          PRIMARY KEY(tool_id) \
+);
+create trigger tool_trigger before insert on `tool` for each row set @last_uuid=uuid(), NEW.tool_id=@last_uuid;
+
+create table `loan` ( \
+	`loan_id` varchar(36) NOT NULL, \
+	`tool_id` varchar(36) NOT NULL, \
+        `loan_to_user_id` varchar(36) NOT NULL, \
+        `loan_datetime` datetime NOT NULL, \
+	PRIMARY KEY (loan_id) \
+);
+create trigger loan_trigger before insert on `loan` for each row set @last_uuid=uuid(), NEW.loan_id=@last_uuid;
+
+create table `competition_category`( \
+          `category_id` varchar(36) NOT NULL, \
+          `name` TEXT NOT NULL, \
+          PRIMARY KEY(category_id) \
+);
+create trigger competition_category_trigger before insert on `competition_category` for each row set @last_uuid=uuid(), NEW.category_id=@last_uuid;
+
+create table `category_group`( \
+          `category_group_id` varchar(36) NOT NULL, \
+          `category_id` varchar(36) NOT NULL, \
+          `group_id` varchar(36) NOT NULL, \
+          PRIMARY KEY(category_group_id) \
+);
+create trigger category_group_trigger before insert on `category_group` for each row set @last_uuid=uuid(), NEW.category_group_id=@last_uuid;
+
+create table `score`( \
+      	`score_id` varchar(36) NOT NULL, \
+        `category_id` varchar(36) NOT NULL, \
+      	`judge_id` varchar(36) NOT NULL, \
+        `group_id` varchar(36) NOT NULL, \
+      	`category_1_score` INT NOT NULL, \
+      	`category_2_score` INT NOT NULL, \
+      	`category_3_score` INT NOT NULL, \
+      	`category_4_score` INT NOT NULL, \
+        PRIMARY KEY(score_id) \
+);
+create trigger score_trigger before insert on `score` for each row set @last_uuid=uuid(), NEW.score_id=@last_uuid;
+
 
 
 -- create tables for wthack_automation_test
